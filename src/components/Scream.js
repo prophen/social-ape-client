82bb6fe246b295d4226abce3bd4c4464db9620cb
Nikema Prophet <nikema.prophet@popschools.com>
2019-09-15 22:46:29 -0700

    delete scream

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom"
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
 import MyButton from "../util/MyButton"
+import DeleteScream from "./DeleteScream"
 // MUI stuff
 import Card from "@material-ui/core/Card"
 import CardContent from "@material-ui/core/CardContent"
@@ -19,6 +20,7 @@ import { connect } from "react-redux"
 import { likeScream, unlikeScream } from "../redux/actions/dataActions"
 const styles = {
   card: {
+    position: "relative",
     display: "flex",
     marginBottom: 20
   },
@@ -58,7 +60,10 @@ class Scream extends Component {
         likeCount,
         commentCount
       },
-      user: { authenticated }
+      user: {
+        authenticated,
+        credentials: { handle }
+      }
     } = this.props
 
     const likeButton = !authenticated ? (
@@ -76,6 +81,10 @@ class Scream extends Component {
         <FavoriteBorder color="primary" />
       </MyButton>
     )
+    const deleteButton =
+      authenticated && userHandle === handle ? (
+        <DeleteScream screamId={screamId} />
+      ) : null
     return (
       <Card className={classes.card}>
         <CardMedia
@@ -93,6 +102,7 @@ class Scream extends Component {
           >
             {userHandle}
           </Typography>
+          {deleteButton}
           <Typography variant="body2" color="textSecondary">
             {dayjs(createdAt).fromNow()}
           </Typography>
--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,4 +1,10 @@
-import { SET_SCREAMS, LIKE_SCREAM, UNLIKE_SCREAM, LOADING_DATA } from "../types"
+import {
+  SET_SCREAMS,
+  LIKE_SCREAM,
+  UNLIKE_SCREAM,
+  LOADING_DATA,
+  DELETE_SCREAM
+} from "../types"
 
 const initialState = {
   screams: [],
@@ -25,6 +31,14 @@ export default function(state = initialState, action) {
         scream => scream.screamId === action.payload.screamId
       )
       state.screams[index] = action.payload
+      return {
+        ...state
+      }
+    case DELETE_SCREAM:
+      index = state.screams.findIndex(
+        scream => scream.screamId === action.payload
+      )
+      state.screams.splice(index, 1)
       return {
         ...state
       }
