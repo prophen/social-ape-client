725cfa989dca3609df19e83183a15aacf5ee37c7
Nikema Prophet <nikema.prophet@popschools.com>
2019-09-21 13:16:06 -0700

    add notifications

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom"
 import { connect } from "react-redux"
 import PropTypes from "prop-types"
 import MyButton from "../../util/MyButton"
+import PostScream from "../scream/PostScream"
+import Notifications from "./Notifications"
 // MUI
 
 import AppBar from "@material-ui/core/AppBar/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
 import Button from "@material-ui/core/Button"
-import PostScream from "../scream/PostScream"
 // Icons
 import HomeIcon from "@material-ui/icons/Home"
-import Notifications from "@material-ui/icons/Notifications"
 
 class Navbar extends Component {
   render() {
@@ -27,9 +27,8 @@ class Navbar extends Component {
                   <HomeIcon />
                 </Link>
               </MyButton>
-              <MyButton tip="Notifications">
-                <Notifications />
-              </MyButton>
+
+              <Notifications />
             </>
           ) : (
             <>
--- src/components/scream/Scream.js
+++ src/components/scream/Scream.js
@@ -85,7 +85,11 @@ class Scream extends Component {
             <ChatIcon color="primary" />
           </MyButton>
           <span>{commentCount} comments</span>
-          <ScreamDialog screamId={screamId} userHandle={userHandle} />
+          <ScreamDialog
+            screamId={screamId}
+            userHandle={userHandle}
+            openDialog={this.props.openDialog}
+          />
         </CardContent>
       </Card>
     )
@@ -95,7 +99,8 @@ class Scream extends Component {
 Scream.propTypes = {
   user: PropTypes.object.isRequired,
   scream: PropTypes.object.isRequired,
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  openDialog: PropTypes.bool
 }
 
 const mapStateToProps = state => ({
